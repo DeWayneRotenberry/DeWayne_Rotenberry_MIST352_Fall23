7367973369babfaa47db1013649095ac7026e139
admin <admin@localhost>
2023-08-24 12:32:56 -0400

    Added GOODBYEWORLD project to the repo.

---
+++ GOODBYEWORLD/src/GOODBYEWORLD.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/** Author: DeWayne Rotenberry
+ * Date: 8/24/2023
+ * This is just a trial
+ */
+public class GOODBYEWORLD {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Print out message 
+System.out.println("Message to display to users");
+
+// Another message for users
+System.out.println("This is another message");
+
+
+	}
+
+
+
+}
