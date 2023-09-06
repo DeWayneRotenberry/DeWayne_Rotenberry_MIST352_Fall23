11e43d556e19e5673c61ece0c47fad699e4d4a01
DeWayneRotenberry <dhr00001@mix.wvu.edu>
2023-09-06 15:26:30 -0400

    hw1

--- Homeworks/HW1A/src/HW1A.java
+++ Homeworks/HW1A/src/HW1A.java
@@ -1,4 +1,22 @@
+//Author: DeWayne Rotenberry
+//Class: MIST352-Fall23
+//HW#1
+// This program prints Hi and sum 
 
+//Should have been HW1A
 public class HW1A {
+	public static void main(String args) {
+		//Should have had ;
+		System.out.print("Hi");
+		
+		int num1= 10;
+		double num2= 5.5;
+		char letter= 'A';
+		//should have been a double not int
+		double sum = num1 + num2;
+		
+		System.out.println("Sum: " + sum);
+		
+	}
 
 }
