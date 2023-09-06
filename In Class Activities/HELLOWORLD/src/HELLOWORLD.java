b2da8dc684ab1ccfe59186cb9780b3e9287c1554
DeWayneRotenberry <dhr00001@mix.wvu.edu>
2023-09-06 14:44:27 -0400

    HelloWorld into in class activities

---
+++ In Class Activities/HELLOWORLD/src/HELLOWORLD.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class HELLOWORLD {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Some statements 
+System.out.println("HELLO WELCOME TO MIST352");
+System.out.println("HELLO WELCOME TO MIST352");
+System.out.println("HELLO WELCOME TO MIST352");
+System.out.println("HELLO WELCOME TO MIST352");
+System.out.println("HELLO WELCOME TO MIST352");
+System.out.println("HELLO WELCOME TO MIST352");
+System.out.println("HELLO WELCOME TO MIST352");
+System.out.println("HELLO WELCOME TO MIST352");
+	}
+
+}
