c0f3eb674e3323dbf8129e93528969c96504a3cd
DeWayneRotenberry <dhr00001@mix.wvu.edu>
2023-09-08 10:25:04 -0400

    HW1B to homeworks

---
+++ Homeworks/HW1B/src/HW1B.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class HW1B {
+	public static void main(String[] args) {
+		
+		System.out.println("Hi ");
+		
+		int num1= 10;
+		double num2= 5.5;
+		char letter= 'A';
+		
+		double sum = num1 + num2;
+		
+		System.out.println("Sum: " + sum);
+		
+	}
+}
