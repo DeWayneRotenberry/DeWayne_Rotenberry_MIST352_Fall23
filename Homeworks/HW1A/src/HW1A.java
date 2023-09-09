fbbf66caf4495ae306443698398de0d7c7dafce4
DeWayneRotenberry <dhr00001@mix.wvu.edu>
2023-09-08 22:17:24 -0400

    Submitted HW1

--- Homeworks/HW1A/src/HW1A.java
+++ Homeworks/HW1A/src/HW1A.java
@@ -6,12 +6,14 @@
 //Should have been HW1A
 public class HW1A {
 	public static void main(String[] args) {
+		
 		//Should have had ;
 		System.out.println("Hi ");
 		
 		int num1= 10;
 		double num2= 5.5;
 		char letter= 'A';
+		
 		//should have been a double not int
 		double sum = num1 + num2;
 		
--- Homeworks/HW1B/src/HW1B.java
+++ Homeworks/HW1B/src/HW1B.java
@@ -10,9 +10,20 @@ public class HW1B {
 		
 		System.out.println("Hi ");
 		
-		int num1= 10;
-		double num2= 5.5;
-		char letter= 'A';
+		int num1;
+		System.out.print("Enter value for num1:\t");
+		num1= 10;
+		System.out.println(num1);
+		
+		double num2;
+		System.out.print("Enter value for num2:\t");
+		num2= 5.5;
+		System.out.println(num2);
+		
+		char letter;
+		System.out.print("Enter value for letter:\t");
+		letter= 'A';
+		System.out.println('A');
 		
 		double sum = num1 + num2;
 		
