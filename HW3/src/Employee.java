6865e069ea2d5d0231aaca4df705c89283f57145
DeWayneRotenberry <dhr00001@mix.wvu.edu>
2023-11-27 19:49:21 -0500

    hw3 updates

--- HW3/src/Employee.java
+++ HW3/src/Employee.java
@@ -7,61 +7,62 @@ public class Employee {
     private int employeeId;
     private String email;
     private double salary;
-    private boolean aboveAvg ;
+    private boolean aboveAvg;
     private String bar;
 
-    // Constructor 
+    // Constructor
     /**
-     * You need to code this
      * This constructor accepts one line from a text file
      * It should split it up and initialize all attributes in this class using the values from the line
-     * For example, name should equal to the first portion of the splited line and dateOfBirth is second and so on.
-     * Lastly, set aboveAvg to false 
+     * For example, name should equal to the first portion of the split line and dateOfBirth is second and so on.
+     * Lastly, set aboveAvg to false
      * @param line
      */
-    public Employee(String line)   		
-    		 {
-
-        setAboveAvg(salary);
+    public Employee(String line) {
+        String[] data = line.split(",");
+        this.name = data[0];
+        this.dateOfBirth = data[1];
+        this.role = data[2];
+        this.employeeId = Integer.parseInt(data[3]);
+        this.email = data[4];
+        this.salary = Double.parseDouble(data[5]);
+        this.aboveAvg = false; // Set initially to false
+        this.bar = SalaryBarChart(salary); // Initialize bar chart
     }
-    
+
     /**
-     * You need to code this
-     * This method accepts the average salary from the main and decide whether this employee's salary is above average or not
+     * This method accepts the average salary from the main and decides whether this employee's salary is above average or not
      * If it is, then set aboveAvg to true
-     * @param intAverage
+     * @param dblAverage
      */
-   public void setAboveAvg(double dblAverage)
-   {
-	   
-	   
-   }
-	
-   /**
-	 * You need to code this.
-	 * This method accepts an employee's salary and returns a bar chart of stars representation of the salary
-	 * For example, 
-	 * 				if the salary is 60,000, then the bar should return 6 stars:  ******
-	 * 				if the salary is 95,600, then the bar should return 9 stars:  *********
-	 * @param salary: the employee salary
-	 * @return bar: representation in start of the salary. Each * represents $10,000
-	 */
-   public String SalaryBarChart(double salary) {
-			
+    public void setAboveAvg(double dblAverage) {
+        aboveAvg = (salary > dblAverage);
+    }
 
-	    	}
-   
-   /**
-    * You need to code this.
-    * This method provides a summary of each employee.
-    * It returns a string formatted to include: employee name, employee ID, the bar chart of the salary, and whether salary is above average or not.
-    * @return : Formatted string of employees basic information.
-    */
-   public String SummerizeEmployee()
-	 
-	 {
-		
-	 }
-	 
+    /**
+     * This method accepts an employee's salary and returns a bar chart of stars representation of the salary
+     * For example,
+     * if the salary is 60,000, then the bar should return 6 stars: ******
+     * if the salary is 95,600, then the bar should return 9 stars: *********
+     * @param salary: the employee salary
+     * @return bar: representation in stars of the salary. Each * represents $10,000
+     */
+    public String SalaryBarChart(double salary) {
+        int stars = (int) (salary / 10000);
+        StringBuilder bar = new StringBuilder();
+        for (int i = 0; i < stars; i++) {
+            bar.append('*');
+        }
+        return bar.toString();
+    }
 
+    /**
+     * This method provides a summary of each employee.
+     * It returns a string formatted to include: employee name, employee ID, the bar chart of the salary, and whether salary is above average or not.
+     * @return : Formatted string of employee's basic information.
+     */
+    public String SummarizeEmployee() {
+        return String.format("Name: %s\nEmployee ID: %d\nSalary Bar Chart: %s\nAbove Average: %b",
+                name, employeeId, bar, aboveAvg);
+    }
 }
