17f795924a51afed1e3f781b119bf87874141c2f
DeWayneRotenberry <dhr00001@mix.wvu.edu>
2023-12-12 17:02:20 -0500

    updates

--- Homeworks/HW4/src/Csv2Arff.java
+++ Homeworks/HW4/src/Csv2Arff.java
@@ -1,6 +1,6 @@
 
-/**
- * This class allows you to convert a csv file to arff file.
+//**
+ /* This class allows you to convert a csv file to arff file.
  * @author MJ
  *
  */
@@ -18,42 +18,31 @@ public class Csv2Arff {
 	}
 	
 	/**
-     * You need to code this
-     * This method accepts the name and location of a csv file
-     * and writes an arff file based on the csv data.
-     * Writing arff file is similar to writing any .txt file.
-     * Just make sure the arff file name ends with .arff
-     *
-     * @param theLocation: The name and location of any csv file
-     */
-    public void Convert2Arff(String theLocation) {
-        // Read CSV file and write ARFF file
-        try (BufferedReader br = new BufferedReader(new FileReader(theLocation));
-             FileWriter fw = new FileWriter(theLocation.replace(".csv", ".arff"))) {
+	 * You need to code this
+	 * This method accepts a name of a csv file and it writes an arff file from the csv file
+	 * Writing arff file is similar to writing any .txt file. Just make sure the arff file name ends with .arff
+	 * @param theLocation: The name and location of any csv file
+	 */
+	public void Convert2Arff(String theLocation)
+	{
+		System.out.println("Done =)");
+			
+	}
+	
+	/**
+	 * This method should red the text file to a two dimensional array of Strings.
+	 * This method should return the data given in a specific row and column in the two dimensional array
+	 * @param theFile: name of the csv file to open 
+	 * @param row: row number in the two dimensional array
+	 * @param column: column number in the two dimensional array
+	 * @return strData2Return: the data in the [column][row]
+	 */
+	public static String RetrieveCell(String theFile, int row, int column)
+	{
+		String strData2Return="";
+		
+		return strData2Return;
+	}
 
-            // TODO: Implement the conversion logic from CSV to ARFF
-
-            System.out.println("Conversion to ARFF is done =)");
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * This method reads the csv file into a two-dimensional array of Strings.
-     * This method returns the data given in a specific row and column in the array.
-     *
-     * @param theFile: name of the csv file to open
-     * @param row:     row number in the two-dimensional array
-     * @param column:  column number in the two-dimensional array
-     * @return strData2Return: the data in the [column][row]
-     */
-    public static String RetrieveCell(String theFile, int row, int column) {
-        String strData2Return = "";
-
-        // TODO: Implement logic to read the CSV file and retrieve data at the specified row and column
-
-        return strData2Return;
-    }
 }
+
